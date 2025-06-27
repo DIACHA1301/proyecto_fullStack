458fe741510a8fa24c7062696e7ba1c989a3b2d0
Diana Charry <dianita_94_11@hotmail.com>
2025-06-26 22:05:19 -0500

    creacion de productos

--- back/src/servidor.js
+++ back/src/servidor.js
@@ -1,12 +1,16 @@
 import express from "express"; // para realizar la conexion con el servidor
 import morgan from "morgan"; // monitorear solicitudes http
+import cors from "cors";
+import routerUsers from "./routers/routerUsers.js";
 
 const servidor = express();
+servidor.use(cors());
 servidor.use(morgan("dev"));
 servidor.use(express.json());
+servidor.use('/users', routerUsers);
 
-servidor.get('/',(solicitud , respuesta) =>{       //puede ser req, resp
-respuesta.status(404).send("No encontrado"); //en caso de que se envie una conexion vacia devuelva un error entonces envie un 404 con un mensaje
+servidor.get('/',(sol , res) =>{       //puede ser req, resp
+res.status(404).send("No encontrado"); //en caso de que se envie una conexion vacia devuelva un error entonces envie un 404 con un mensaje
 })
 
 export default servidor
